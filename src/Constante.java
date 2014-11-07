f6c826346b2e2b8ba1951845c26a79e634aa9279
Dowan <thomas.gosselin@etudiant.univ-lille1.fr>
2014-11-07 11:17:29 +0100

    maintenant je sais quoi mettre dedans

--- src/Constante.java
+++ src/Constante.java
@@ -1,4 +1,6 @@
-
 public class Constante {
- // voir ce qu'il faut mettre dedans
+	public static Coordonnees haut = new Coordonnees(0, -1);
+	public static Coordonnees bas = new Coordonnees(0, 1);
+	public static Coordonnees gauche = new Coordonnees(-1, 0);
+	public static Coordonnees droite = new Coordonnees(1, 0);
 }
